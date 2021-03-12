e699de4cea505fecf307eefd4422aaf5ac778666
assansanogo <assansanogo@gmail.com>
2021-03-12 16:33:51 +0100

    javascript to capture mouse movement

---
+++ browser_heatmap/browser_mouse_posiiton.js
@@ -0,0 +1,63 @@
+(function() {
+
+    document.onmousemove = handleMouseMove;
+    let csvContent = "data:text/csv;charset=utf-8,";
+    document.onclick = handleMouseClick;
+
+
+    let
+    body    = document.querySelector('body'),
+    beginPath = false;
+
+
+
+
+
+
+
+    let started = false;
+
+    function handleMouseMove(event) {
+        var eventDoc, doc, body;
+
+        event = event || window.event; // IE-ism
+        if (event.pageX != null & started== false) {
+            started = true;
+            alert(`the script is recording your mouse positions, when you click it will automatically download the mouse positions`);
+            window.resizeTo(500,500);
+        }
+
+        // If pageX/Y aren't available and clientX/Y are,
+        // calculate pageX/Y - logic taken from jQuery.
+        // (This is to support old IE)
+        if (event.pageX == null && event.clientX != null) {
+            eventDoc = (event.target && event.target.ownerDocument) || document;
+            doc = eventDoc.documentElement;
+            body = eventDoc.body;
+
+            event.pageX = event.clientX +
+              (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+              (doc && doc.clientLeft || body && body.clientLeft || 0);
+            event.pageY = event.clientY +
+              (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+              (doc && doc.clientTop  || body && body.clientTop  || 0 );
+        }
+        if (event.pageX != null) 
+        {
+         let string_pos = [event.pageX,event.pageY,window.pageXOffset,window.pageYOffset, window.innerHeight, window.innerWidth].join(",");
+         console.log(string_pos);
+         csvContent += string_pos + "\r\n";
+        }
+        // Use event.pageX / event.pageY here
+    }
+
+
+
+    function handleMouseClick(event) {
+        var eventDoc, doc, body;
+
+        event = event || window.event;
+        var encodedUri = encodeURI(csvContent);
+        window.open(encodedUri);
+    }
+})();
